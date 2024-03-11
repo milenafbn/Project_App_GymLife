fbbad463c07d928ba89111c2c4a572bc49348fae
Milena Neves <80131766+milenafbn@users.noreply.github.com>
2024-03-10 22:49:46 -0300

    Add base colors

---
+++ lib/core/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Cores {
+  static const Color primary = Color.fromARGB(255, 126, 87, 194);
+  static const Color primarylight = Color.fromARGB(255, 148, 121, 194);
+  static const Color primarydark = Color.fromRGBO(94, 63, 163, 1);
+}
