5276497d5e3ead317cec1b7b407731d4f4397b19
Milena Neves <80131766+milenafbn@users.noreply.github.com>
2024-03-13 00:54:51 -0300

    Edit Splash Screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -14,6 +14,7 @@ class _SplashScreenState extends State<SplashScreen>
 
   @override
   void initState() {
+    super.initState();
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive); //some com a barra de status
 
     Future.delayed(const Duration(seconds: 2), () {
@@ -31,24 +32,43 @@ class _SplashScreenState extends State<SplashScreen>
     super.dispose(); //reaparece a barra de status
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: RadialGradient(
-            colors: [Color.fromRGBO(148, 121, 194, 1), Color.fromRGBO(126, 87, 194, 1)],
-            center: Alignment.center,
-            radius: 1.0,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: RadialGradient(
+                colors: [Color.fromRGBO(148, 121, 194, 1), Color.fromRGBO(126, 87, 194, 1)],
+                center: Alignment.center,
+                radius: 1.0,
+              ),
+            ),
+            // child: const Center(
+            //   child: CircularProgressIndicator(
+            //     valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            //   ),
+            // ),
           ),
-        ),
-        child: const Center(
-          child: CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset("assets/deep_purple_bow.png", height: 150),
+                const SizedBox(height: 15),
+                const Text("Hans+", 
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 38, 
+                    color: Colors.white,
+                  ),
+                ),
+                ],
+            ),
           ),
-        ),
-        
+        ],
       ),
     );
   }
