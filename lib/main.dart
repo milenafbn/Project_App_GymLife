3d3639f6172b338cdd115ae406e4879b2aa5de84
Milena Neves <80131766+milenafbn@users.noreply.github.com>
2024-03-29 23:52:29 -0300

    Update import statements and add RoteadorTela class

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:gymlife/screens/home.dart';
 import 'firebase_options.dart';
-import 'package:projeto_app_academia/screens/exercicio_tela.dart';
-import 'package:projeto_app_academia/screens/autenticacao_tela.dart';
-import 'package:projeto_app_academia/screens/splash_screen.dart';
+import 'package:gymlife/screens/exercicio_tela.dart';
+import 'package:gymlife/screens/autenticacao_tela.dart';
+import 'package:gymlife/screens/splash_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,8 +23,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SplashScreen(),
+      home: RoteadorTela(),
     );
+  }
+}
+
+class RoteadorTela extends StatelessWidget {
+  const RoteadorTela({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.userChanges(), //observar a stream de usuário quando houver mudanças
+      builder: (context, snapshot) {
+        if (snapshot.hasData) { //se tiver informação o usuário está logado
+          return HomePage(user: snapshot.data!); //passa o usuário logado para a tela home
+        } else {
+          return const AutenticacaoTela();
+        }
+      },
+    );  //observa o stream e chama o builder
   }
 }
 
